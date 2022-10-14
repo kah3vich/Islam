f3d35a9970f2b97a64a8cfb180d905188536eec3
kah3vich <nik.b20@mail.ru>
2022-10-14 09:18:02 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Index: [click me](https://kah3vich.github.io/Islam/public/index.html)
+
+# Volunteers: [click me](https://kah3vich.github.io/Islam/public/volunteers.html)
---
+++ src/js/main/index.js
@@ -0,0 +1,6 @@
+const volunteersSwiper = new Swiper('.volunteersSwiper', {
+	navigation: {
+		nextEl: '.volunteers-recom__slider .swiper-controls .swiper-button-next',
+		prevEl: '.volunteers-recom__slider .swiper-controls .swiper-button-prev'
+	}
+})

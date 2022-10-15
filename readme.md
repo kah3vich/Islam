bc3a1d754751cf010659e1ac4d6820b51070d7a2
kah3vich <nik.b20@mail.ru>
2022-10-16 00:43:49 +0300

    feat: contributors page

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Index: [click me](https://kah3vich.github.io/Islam/public/index.html)
 
 # Volunteers: [click me](https://kah3vich.github.io/Islam/public/volunteers.html)
+
+# Contributors: [click me](https://kah3vich.github.io/Islam/public/contributors.html)
--- src/js/main/index.js
+++ src/js/main/index.js
@@ -3,8 +3,8 @@
 const volunteersSwiper = new Swiper('.volunteersSwiper', {
 	effect: 'fade',
 	navigation: {
-		nextEl: '.volunteers-recom__slider .swiper-controls .swiper-button-next',
-		prevEl: '.volunteers-recom__slider .swiper-controls .swiper-button-prev'
+		nextEl: '.banners__slider .swiper-controls .swiper-button-next',
+		prevEl: '.banners__slider .swiper-controls .swiper-button-prev'
 	}
 })
 
@@ -108,5 +108,159 @@ $(() => {
 				$this.val($(this).data('val')).trigger('change')
 				$next.find('.c_select-placeholder').trigger('click')
 			})
+	})
+})
+
+//| Accordion
+
+$('.accordion__block').on('click', function () {
+	if ($(this).hasClass('active')) {
+		$(this).removeClass('active')
+	} else {
+		$(this).addClass('active')
+	}
+})
+
+//|  Pagination
+
+var items = $('.contributors__list .contributors__item')
+var numItems = items.length
+var perPage = 6
+
+// items.slice(perPage).hide()
+
+// $('#pagination-controls').pagination({
+// 	items: numItems,
+// 	itemsOnPage: perPage,
+// 	prevText: '&laquo;',
+// 	nextText: '&raquo;',
+// 	onPageClick: function (pageNumber) {
+// 		var showFrom = perPage * (pageNumber - 1)
+// 		var showTo = showFrom + perPage
+// 		items.hide().slice(showFrom, showTo).show()
+// 	}
+// })
+
+// Returns an array of maxLength (or less) page numbers
+// where a 0 in the returned array denotes a gap in the series.
+// Parameters:
+//   totalPages:     total number of pages
+//   page:           current page
+//   maxLength:      maximum size of returned array
+function getPageList(totalPages, page, maxLength) {
+	if (maxLength < 5) throw 'maxLength must be at least 5'
+
+	function range(start, end) {
+		return Array.from(Array(end - start + 1), (_, i) => i + start)
+	}
+
+	var sideWidth = maxLength < 9 ? 1 : 2
+	var leftWidth = (maxLength - sideWidth * 2 - 3) >> 1
+	var rightWidth = (maxLength - sideWidth * 2 - 2) >> 1
+	if (totalPages <= maxLength) {
+		// no breaks in list
+		return range(1, totalPages)
+	}
+	if (page <= maxLength - sideWidth - 1 - rightWidth) {
+		// no break on left of page
+		return range(1, maxLength - sideWidth - 1)
+			.concat([0])
+			.concat(range(totalPages - sideWidth + 1, totalPages))
+	}
+	if (page >= totalPages - sideWidth - 1 - rightWidth) {
+		// no break on right of page
+		return range(1, sideWidth)
+			.concat([0])
+			.concat(range(totalPages - sideWidth - 1 - rightWidth - leftWidth, totalPages))
+	}
+	// Breaks on both sides
+	return range(1, sideWidth)
+		.concat([0])
+		.concat(range(page - leftWidth, page + rightWidth))
+		.concat([0])
+		.concat(range(totalPages - sideWidth + 1, totalPages))
+}
+
+$(function () {
+	// Number of items and limits the number of items per page
+	var numberOfItems = $('.contributors__list  .contributors__item').length
+	var limitPerPage = 8
+	// Total pages rounded upwards
+	var totalPages = Math.ceil(numberOfItems / limitPerPage)
+	// Number of buttons at the top, not counting prev/next,
+	// but including the dotted buttons.
+	// Must be at least 5:
+	var paginationSize = 7
+	var currentPage
+
+	function showPage(whichPage) {
+		if (whichPage < 1 || whichPage > totalPages) return false
+		currentPage = whichPage
+		$('.contributors__list  .contributors__item')
+			.hide()
+			.slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage)
+			.show()
+		// Replace the navigation items (not prev/next):
+		$('#pagination-controls li').slice(1, -1).remove()
+		getPageList(totalPages, currentPage, paginationSize).forEach(item => {
+			$('<li>')
+				.addClass(
+					'page-item ' + (item ? 'current-page ' : '') + (item === currentPage ? 'active ' : '')
+				)
+				.append(
+					$('<a>')
+						.addClass('page-link')
+						.attr({
+							href: 'javascript:void(0)'
+						})
+						.text(item || '...')
+				)
+				.insertBefore('#next-page')
+		})
+		return true
+	}
+
+	// Include the prev/next buttons:
+	$('#pagination-controls').append(
+		$('<li>')
+			.addClass('page-item')
+			.attr({ id: 'previous-page' })
+			.append(
+				$('<a>')
+					.addClass('page-link')
+					.attr({
+						href: 'javascript:void(0)'
+					})
+					.text('Prev')
+			),
+		$('<li>')
+			.addClass('page-item')
+			.attr({ id: 'next-page' })
+			.append(
+				$('<a>')
+					.addClass('page-link')
+					.attr({
+						href: 'javascript:void(0)'
+					})
+					.text('Next')
+			)
+	)
+	// Show the page links
+	$('.contributors__list ').show()
+	showPage(1)
+
+	// Use event delegation, as these items are recreated later
+	$(document).on('click', '#pagination-controls li.current-page:not(.active)', function () {
+		return showPage(+$(this).text())
+	})
+	$('#next-page').on('click', function () {
+		return showPage(currentPage + 1)
+	})
+
+	$('#previous-page').on('click', function () {
+		return showPage(currentPage - 1)
+	})
+	$('#pagination-controls').on('click', function () {
+		$('html,body').animate({ scrollTop: 0 }, 0)
 	})
 })
